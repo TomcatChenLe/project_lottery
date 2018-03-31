5f8e30a75282c320091665c60cc41dca022451a9
ChenLe <1245964652@qq.com>
2018-03-31 16:00:02 +0800

    greffier

---
+++ greffier.h
@@ -0,0 +1,13 @@
+#ifndef _GREFFIER_H_
+#define _GREFFIER_H_
+
+void greffier_desk(user_link,issue_link,buy_link);//公证员界面
+void open_win(user_link,issue_link,buy_link);//开奖界面
+void publish_num(issue_link);//发行中奖号码
+void give_money(user_link,issue_link,buy_link);//发放奖金
+void display_lottery(issue_link,buy_link);//查看彩票信息界面
+void display_issue(issue_link);//查看彩票发行记录
+void display_buy(buy_link);//查看购买彩票记录
+
+
+#endif
